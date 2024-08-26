a67a551855cc6df14e1a73575c4045f8f835aed7
asantos <thesantos395@gmail.com>
2024-08-26 16:40:20 +0800

    working linke

--- LinkedList/linked.js
+++ LinkedList/linked.js
@@ -1,48 +1,74 @@
-/*
-const linkedList = (function () {
-
-    const addName = (name) => {
-
-        return {name}
-    }
-
-
-
-
-
-    return {}
-})()
-
-const node = (function () {
-    
-    const createNode = () => {
-        const value = null
-        const nextNode = null
-
-        return {value, nextNode}
-    }
-
-
-
-
-    return {}
-})()
-*/
-
 const linkedList = (list) => {
+    let head = null
+    let tail = null
+
+    const prepend = (value) =>   {
+        if (head === null){
+        head = createNode(value)
+        } else {
+            let temp = head;
+            head = createNode(value)
+            head.setNext(temp)
+        }
+    }
+
+    const append = (value) =>   {
+        if (head === null) {
+            head = createNode(value)
+        }else {
+        let temp = head
+        while (temp.getNext() !==null) temp = temp.getNext()
+        temp.setNext(createNode(value))
+        }
+    }
+
+    const toString = () => {
+        let temp = head
+        let string = ""
+        while (temp.getNext() !==null) {
+            string = string + `(${temp.getValue()}) -> `
+            temp = temp.getNext()
+            if (temp.getNext() === null) string = string +  `(${temp.getValue()}) -> `  + 'null' 
+        }
+        return string
+    }
 
     
 
-    return {list}
+    return {list,prepend,append,toString}
 }
 
 
-const createNode = () => {
-    const value = null
-    const nextNode = null
+const createNode = (val) => {
+    let value = val
+    let nextNode = null
 
     const setValue = (input) => value = input
-    const setNextNode = (input) => nextNode = input
+    const setNext = (input) => nextNode = input
 
-    return {value, nextNode}
+    const getValue = () =>  value 
+    const getNext = () => nextNode 
+
+    return {value, nextNode, setValue, setNext, getValue, getNext}
 }
+
+
+const list =  linkedList("list")
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString())
+
+
+
+
+/*
+const Dog = createNode();
+Dog.setValue("cat")
+console.log(Dog.getValue())
+*/
